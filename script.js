e12b01c16d97fc79b6d61a93e5a8bc26a28b0155
MotekuMagolo <2679354@students.wits.ac.za>
2026-03-02 16:33:06 +0200

    Updated JS

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function searchCountry(countryName) {
     
     try {
         
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         
         if (!response.ok) {
             throw new Error('Country not found. Please try again.');   
